638662e082ee83fa34209d0d0d17df5df3a3e10e
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2021-01-28 15:10:04 +0700

    app

--- wa.js
+++ wa.js
@@ -12,7 +12,7 @@ if (fs.existsSync(SESSION_FILE_PATH)) {
     sessionCfg = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+const client = new Client({ puppeteer: { headless: true, args: ['--no-sandbox'] }, session: sessionCfg });
 // const client = new Client();
 
 client.initialize();
